25a1d63a3463f93c700e23986340c47e3edeabf5
Quy Tran <quytran288@gmail.com>
2017-08-07 00:05:03 -0500

    Apply multiple phases for transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,43 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
     trigger('divState', [
       state('normal', style({
         backgroundColor: 'red',
-        transform: 'translateX(0)'
+        transform: 'translateX(0) scale(1)'
       })),
       state('highlighted', style({
         backgroundColor: 'blue',
-        transform: 'translateX(100px)'
+        transform: 'translateX(100px) scale(1)'
       })),
-      transition('normal <=> highlighted', animate(300)),
-      // transition('highlighted => normal', animate(800))
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)), // transition from normal state to highlighted state
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [  // using wildcard to indicate all states & use multiple phases
+        style({ // immediately change
+          backgroundColor: 'orange'
+        }),
+        animate(1000, style({ // change with animate
+          borderRadius: '50px'
+        })),
+        animate(500) // animation for end phase
+      ])
     ])
   ]
 })
 
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
 	list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState == 'normal' ? this.wildState = 'shrunken' : this.wildState = 'normal';
   }
 
 	onAdd(item) {
